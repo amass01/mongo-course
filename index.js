fde371723b3ff603724fe1c602da14c3b0ada22f
Amir Massarwi <amassarwi@gmail.com>
2018-01-10 22:19:40 +0100

    * Install express * Install mongodb * Install consolidate * Initiate index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express'),
+      engines = require('consolidate'),
+      mongoClient = require('mongodb').MongoClient,
+      assert = require('assert');
+
+const app = express();
+
